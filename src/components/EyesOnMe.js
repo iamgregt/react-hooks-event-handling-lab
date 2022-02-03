94c0c5ada76d1201b018ba2e945a70b657dd16a9
GregT <taylor.gregory2@gmail.com>
2022-02-02 20:40:24 -0700

    Save

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe({focus}){
+    return(
+        <button focus={focus}>Eyes on me</button>
+    )
+}
+
+
+function focus(e){
+    if(e === onblur){
+        console.log("Hey! Eyes on me!")
+    }else{
+        console.log("Good!")
+    }
+}
+
+export default EyesOnMe
